70bc737edc828b5086195287c1677b127e4b7d74
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-07-04 10:58:42 -0400

    update turelli_model description

--- R/turelli_model.R
+++ R/turelli_model.R
@@ -1,5 +1,11 @@
 #' Turelli Model Function
 #'
+#' @description
+#' Implements the iteration
+#'
+#' \deqn{p_{t+1} = \frac{p_t F (1 - \mu)}{1 + p_t (F - 1 - s_h) + p_t^2 s_h (1 - \mu F)}}
+#'
+#'
 #' @param p_t A numeric value between 0 and 1
 #' @param F_val A numeric value greater than 0
 #' @param mu_val A numeric value between 0 and 1
