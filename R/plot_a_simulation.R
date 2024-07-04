359134cd8774648780a281c5c8dc382d8e32dec3
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-07-04 12:41:09 -0400

    add plot function

---
+++ R/plot_a_simulation.R
@@ -0,0 +1,27 @@
+#' Plot a simulation
+#'
+#' @description
+#' Plots a simulation
+#'
+#' @param p_t_vec A numeric vector
+#' @param threshold A numeric value
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @examples
+#' plot_a_simulation(runif(1000))
+plot_a_simulation <- function(p_t_vec,threshold=0.0001){
+
+  y_1 <- base::min(p_t_vec)
+  y_2 <- base::max(p_t_vec)
+
+  p_t_vec_trunc <- p_t_vec[p_t_vec > threshold]
+
+  ggplot2::ggplot(tibble::tibble(p_t=p_t_vec_trunc),ggplot2::aes(x=1:length(p_t),y=p_t)) +
+    ggplot2::geom_point(alpha=0.2,color="#4988BFFF") +
+    ggplot2::labs(x="Generation",y=latex2exp::TeX("$p_{t}$")) +
+    ggplot2::theme_bw(base_size=14) +
+    ggplot2::ylim(c(y_1,y_2))
+
+}
