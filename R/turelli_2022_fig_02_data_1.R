1cf60c1fccadb8b74d2c2f76918975eafd5a5860
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-07-06 13:34:46 -0400

    add fig 2 data part 1

---
+++ R/turelli_2022_fig_02_data_1.R
@@ -0,0 +1,17 @@
+#' Turelli et al. 2022 Figure 2 F_m = 1.021 data
+#'
+#' Model-simulated data for Figure 2 of Turelli et al. 2022.
+#' This data corresponds to the case where the mean value of
+#' F is 1.021 for the log-normal distribution of F.
+#'
+#' @format ## `turelli_2022_fig_02_data_1`
+#' A data frame with 200 rows and 4 columns:
+#' \describe{
+#'   \item{F_val_m}{Median F value}
+#'   \item{sh_val}{CI value}
+#'   \item{repeat_num}{Repitition number}
+#'   \item{t_f}{Persistence time}
+#'   ...
+#' }
+#' @source Simulated from a model following Turelli et al. 2022
+"turelli_2022_fig_02_data_1"
