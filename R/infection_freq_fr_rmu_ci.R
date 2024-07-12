3874edb793eebdb710c96b32ef3a816b7019856a
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-07-12 15:53:57 -0400

    update ci models

---
+++ R/infection_freq_fr_rmu_ci.R
@@ -0,0 +1,72 @@
+#' Iterate Random F and mu infection frequency model with CI
+#'
+#' @param F_val_m A numeric value for the mean fitness
+#' @param F_cv A numeric value for the coefficient of variation for the fitness
+#' @param mu_vect A numeric vector of transmission rates for each group
+#' @param bin_props A numeric vector of proportions for each group
+#' @param s_h A numeric value for the CI
+#' @param N_val A numeric value for the total population size
+#' @param p_t_init A numeric value between 0 and 1 for the initial infection frequency
+#' @param n_iter A numeric value for the maximum number of iterations
+#' @param lwr_thresh A numeric value for the lower threshold
+#' @param upr_thresh A numeric value for the upper threshold
+#'
+#' @return A numeric vector of infection frequencies
+#' @export
+#'
+#' @examples
+#' ptlt <- infection_freq_rf_rmu_ci_iteration(1.2,0.1,c(0.01,0.9),c(0.99,0.01),0.1,1000)
+#' plot_a_simulation(ptlt,alpha=0.8)
+#' extract_stats(ptlt)
+infection_freq_rf_rmu_ci_iteration <- function(F_val_m,F_cv,mu_vect,bin_props,s_h,N_val,
+                                            p_t_init=0.4,n_iter=10000,
+                                            lwr_thresh = 0.0001, upr_thresh = 0.99){
+
+  F_val_s <- stats::rnorm(n_iter, mean=log(F_val_m), sd=sqrt(log(F_cv^2 + 1))) |>
+    exp()
+  F_val <- base::ifelse(F_val_s < 0.001, 0.001, F_val_s)
+
+  p_t <- p_t_init
+  p_t_vec <- base::numeric(n_iter)
+  p_t_vec[1] <- p_t
+  i_ind <- 2
+
+  while(i_ind <= n_iter && p_t > lwr_thresh && p_t < upr_thresh){
+    p_t <- infection_freq_rmu_ci(p_t,F_val[i_ind-1],mu_vect,bin_props,s_h,N_val)
+    p_t_vec[i_ind] <- p_t
+    i_ind <- i_ind + 1
+  }
+
+  return(p_t_vec[p_t_vec > 0])
+
+}
+
+
+#' Main simulation for random F and mu model with CI
+#'
+#' @description
+#' This function simulates the infection frequency
+#' for random fitness and mu values
+#'
+#' @param rep_num A numeric value for the number of replications
+#' @param F_val_m A numeric value for the mean fitness
+#' @param F_cv A numeric value for the coefficient of variation for the fitness
+#' @param mu_vect A numeric vector of transmission rates for each group
+#' @param bin_props A numeric vector of proportions for each group
+#' @param s_h A numeric value for the CI
+#' @param N_val A numeric value for the total population size
+#' @param ... Additional arguments
+#'
+#' @return A tibble of simulation results
+#' @export
+#'
+#' @examples
+#' infection_freq_rf_rmu_ci_sim(1,1.2,0.1,c(0.01,0.9),c(0.99,0.01),0.1,1000)
+infection_freq_rf_rmu_ci_sim <- function(rep_num,F_val_m,F_cv,mu_vect,bin_props,s_h,N_val,...){
+
+  a_sim <- infection_freq_rf_rmu_ci_iteration(F_val_m,F_cv,mu_vect,bin_props,s_h,N_val)
+  sim_res <- extract_stats(a_sim)
+  parm_data <- tibble::tibble(rep_num=rep_num,F_val_m=F_val_m,F_cv=F_cv,mu_vect=mu_vect,bin_props=bin_props,s_h=s_h,N_val=N_val)
+  return(dplyr::bind_cols(parm_data,sim_res))
+
+}
