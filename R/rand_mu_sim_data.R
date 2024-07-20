dc1d4e6bbf9505a12cf9dbbe7a2ba4b99061a559
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-07-20 08:48:18 -0400

    update data documentation

---
+++ R/rand_mu_sim_data.R
@@ -0,0 +1,50 @@
+#' Turelli et al. 2022 Figure 2 F_m = 1.021 data
+#'
+#' Model-simulated data for multi-binomial distributed mu.
+#'
+#'
+#'
+#' @format ## `rand_mu_sim_data_1`
+#' A data frame with 18,200 rows and 12 columns:
+#' \describe{
+#'   \item{rep_num}{Repitition number}
+#'   \item{F_val}{F value}
+#'   \item{mu_vect}{mu value}
+#'   \item{bin_props}{group proportion for mu}
+#'   \item{N_val}{Population size}
+#'   \item{p_t_median}{median infection proportion}
+#'   \item{p_t_mean}{mean infection proportion}
+#'   \item{p_t_var}{variance of infection proportion}
+#'   \item{p_t_min}{minimum infection proportion}
+#'   \item{p_t_max}{maximum infection proportion}
+#'   \item{gen_steps}{number of generations}
+#'   \item{final_p_t}{final infection proportion}
+#'   ...
+#' }
+#' @source Simulated from a model.
+"rand_mu_sim_data_1"
+
+#' Turelli et al. 2022 Figure 2 F_m = 1.05 data
+#'
+#' Model-simulated data for multi-binomial distributed mu.
+#'
+#'
+#' @format ## `rand_mu_sim_data_2`
+#' A data frame with 72,800 rows and 12 columns:
+#' \describe{
+#'   \item{rep_num}{Repitition number}
+#'   \item{F_val}{F value}
+#'   \item{mu_vect}{mu value}
+#'   \item{bin_props}{group proportion for mu}
+#'   \item{N_val}{Population size}
+#'   \item{p_t_median}{median infection proportion}
+#'   \item{p_t_mean}{mean infection proportion}
+#'   \item{p_t_var}{variance of infection proportion}
+#'   \item{p_t_min}{minimum infection proportion}
+#'   \item{p_t_max}{maximum infection proportion}
+#'   \item{gen_steps}{number of generations}
+#'   \item{final_p_t}{final infection proportion}
+#'   ...
+#' }
+#' @source Simulated from a model.
+"rand_mu_sim_data_2"
