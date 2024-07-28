d22fd0e61eba720acf5e841e77012747d87b6b66
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-07-28 15:03:26 -0400

    add fixed F ci data

---
+++ R/rand_mu_ci_sim_data.R
@@ -0,0 +1,105 @@
+#' Simulation data for random mu model with CI.
+#'
+#' Model-simulated data for multi-binomial distributed mu
+#' Single mu group scenario with CI at 0.1.
+#'
+#'
+#'
+#' @format ## `rand_mu_ci_01_sim_data_1`
+#' A data frame with 27,500 rows and 13 columns:
+#' \describe{
+#'   \item{rep_num}{Repitition number}
+#'   \item{F_val}{F value}
+#'   \item{bin_props}{group proportion for mu}
+#'   \item{s_h}{CI value}
+#'   \item{N_val}{Population size}
+#'   \item{p_t_median}{median infection proportion}
+#'   \item{p_t_mean}{mean infection proportion}
+#'   \item{p_t_var}{variance of infection proportion}
+#'   \item{p_t_min}{minimum infection proportion}
+#'   \item{p_t_max}{maximum infection proportion}
+#'   \item{gen_steps}{number of generations}
+#'   \item{final_p_t}{final infection proportion}
+#'   ...
+#' }
+#' @source Simulated from a model.
+"rand_mu_ci_01_sim_data_1"
+
+#' Simulation data for random mu model with CI.
+#'
+#' Model-simulated data for multi-binomial distributed mu
+#' Two mu groups scenario with CI at 0.1.
+#'
+#'
+#' @format ## `rand_mu_ci_01_sim_data_2`
+#' A data frame with 110,000 rows and 13 columns:
+#' \describe{
+#'   \item{rep_num}{Repitition number}
+#'   \item{F_val}{F value}
+#'   \item{bin_props}{group proportion for mu}
+#'   \item{s_h}{CI value}
+#'   \item{N_val}{Population size}
+#'   \item{p_t_median}{median infection proportion}
+#'   \item{p_t_mean}{mean infection proportion}
+#'   \item{p_t_var}{variance of infection proportion}
+#'   \item{p_t_min}{minimum infection proportion}
+#'   \item{p_t_max}{maximum infection proportion}
+#'   \item{gen_steps}{number of generations}
+#'   \item{final_p_t}{final infection proportion}
+#'   ...
+#' }
+#' @source Simulated from a model.
+"rand_mu_ci_01_sim_data_2"
+
+#' Simulation data for random mu model with CI.
+#'
+#' Model-simulated data for multi-binomial distributed mu
+#' Single mu group scenario with CI at 0.45.
+#'
+#'
+#'
+#' @format ## `rand_mu_ci_045_sim_data_1`
+#' A data frame with 27,500 rows and 13 columns:
+#' \describe{
+#'   \item{rep_num}{Repitition number}
+#'   \item{F_val}{F value}
+#'   \item{bin_props}{group proportion for mu}
+#'   \item{s_h}{CI value}
+#'   \item{N_val}{Population size}
+#'   \item{p_t_median}{median infection proportion}
+#'   \item{p_t_mean}{mean infection proportion}
+#'   \item{p_t_var}{variance of infection proportion}
+#'   \item{p_t_min}{minimum infection proportion}
+#'   \item{p_t_max}{maximum infection proportion}
+#'   \item{gen_steps}{number of generations}
+#'   \item{final_p_t}{final infection proportion}
+#'   ...
+#' }
+#' @source Simulated from a model.
+"rand_mu_ci_045_sim_data_1"
+
+#' Simulation data for random mu model with CI.
+#'
+#' Model-simulated data for multi-binomial distributed mu
+#' Two mu groups scenario with CI at 0.45.
+#'
+#'
+#' @format ## `rand_mu_ci_045_sim_data_2`
+#' A data frame with 110,000 rows and 13 columns:
+#' \describe{
+#'   \item{rep_num}{Repitition number}
+#'   \item{F_val}{F value}
+#'   \item{bin_props}{group proportion for mu}
+#'   \item{s_h}{CI value}
+#'   \item{N_val}{Population size}
+#'   \item{p_t_median}{median infection proportion}
+#'   \item{p_t_mean}{mean infection proportion}
+#'   \item{p_t_var}{variance of infection proportion}
+#'   \item{p_t_min}{minimum infection proportion}
+#'   \item{p_t_max}{maximum infection proportion}
+#'   \item{gen_steps}{number of generations}
+#'   \item{final_p_t}{final infection proportion}
+#'   ...
+#' }
+#' @source Simulated from a model.
+"rand_mu_ci_045_sim_data_2"
