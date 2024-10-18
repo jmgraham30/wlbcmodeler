b1d0d079bdaf5c95706551abe7e8cd0852906c75
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-10-18 09:58:11 -0400

    update extract_stats

--- R/extract_stats.R
+++ R/extract_stats.R
@@ -5,23 +5,36 @@
 #'
 #' @param p_t_vec A numeric vector
 #' @param threshold A numeric value greater than 0
+#' @param burn_in A numeric value greater than 0
 #'
 #' @return A tibble
 #' @export
 #'
 #' @examples
 #' extract_stats(runif(1000))
-extract_stats <- function(p_t_vec,threshold=0.0001){
+extract_stats <- function(p_t_vec,threshold=0.0001,burn_in=500){
 
   p_t_vec_trunc <- p_t_vec[p_t_vec > threshold]
 
-  p_t_median <- stats::median(p_t_vec_trunc)
-  p_t_mean <- base::mean(p_t_vec_trunc)
-  p_t_var <- stats::var(p_t_vec_trunc)
-  p_t_min <- base::min(p_t_vec_trunc)
-  p_t_max <- base::max(p_t_vec_trunc)
   gen_steps <- base::length(p_t_vec_trunc)
-  final_p_t <- p_t_vec_trunc[gen_steps]
+
+  if (gen_steps > burn_in) {
+    p_t_vec_s <- p_t_vec_trunc[(burn_in+1):length(p_t_vec_trunc)]
+    p_t_median <- stats::median(p_t_vec_s)
+    p_t_mean <- base::mean(p_t_vec_s)
+    p_t_var <- stats::var(p_t_vec_s)
+    p_t_min <- base::min(p_t_vec_s)
+    p_t_max <- base::max(p_t_vec_s)
+    final_p_t <- p_t_vec_trunc[gen_steps]
+  } else {
+    p_t_median <- NA
+    p_t_mean <- NA
+    p_t_var <- NA
+    p_t_min <- NA
+    p_t_max <- NA
+    final_p_t <- NA
+  }
+
 
   return(tibble::tibble(p_t_median=p_t_median,
                         p_t_mean=p_t_mean,
